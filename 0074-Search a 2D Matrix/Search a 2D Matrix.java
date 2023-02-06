19e020c9c0f43259df5e4d10ad2a3bfe08225204
Karthick R <121752013+karthick-rajendiran@users.noreply.github.com>
2023-02-06 18:55:18 +0530

    Create Search a 2D Matrix.java

---
+++ 0074-Search a 2D Matrix/Search a 2D Matrix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length==0)    return false;
+        int row=matrix.length;
+        int col=matrix[0].length;
+        int low=0, high=(row*col)-1;
+        while(low<=high) {
+            int mid=low+(high-low)/2;
+            if(matrix[mid/col][mid%col]==target)    
+              return true;
+            if(matrix[mid/col][mid%col]<target)
+                low=mid+1;
+            else    
+              high=mid-1;
+        }
+        return false;
+    }
+}
